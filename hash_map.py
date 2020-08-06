7acb71c510010ca3a77cb2d8d297eb1c0b7e00eb
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2020-08-06 10:25:29 -0700

    finished implementation of hash_map

--- hash_map.py
+++ hash_map.py
@@ -1,10 +1,7 @@
 # Course: CS 261 - Data Structures
 # Student Name: Mallory Huston
 # Assignment: 5, Part 1
-# Description: Implementation of a hash map. This hash map uses a hash table of buckets,
-#              each containing a linked list of hash links. Each hash link stores the
-#              key-value pair (string and object in this case) and a pointer to the next
-#              link in the list.
+# Description: Implementation of a hash map data structure.
 
 
 # Import pre-written DynamicArray and LinkedList classes
@@ -61,60 +58,151 @@ class HashMap:
 
     def clear(self) -> None:
         """
-        Clears the content of the hash map
+        Clears the contents of the HashMap
         """
-        pass
+        # iterate through HashMap.buckets and replace current LL with empty LL
+        for index in range(self.capacity):
+            self.buckets.set_at_index(index, LinkedList())
+
+        # update size property to zero
+        self.size = 0
 
     def get(self, key: str) -> object:
         """
         Returns the value associated with the given key
         """
-        return None
+        # pass key through hash function to find appropriate index in DA
+        hash_val = self.hash_function(key)
+        da_index = hash_val % self.capacity
+
+        # handle case where HashMap is empty
+        if self.buckets.length() == 0:
+            return False
+
+        # reference target LL with convenient variable
+        target_ll = self.buckets.get_at_index(da_index)
+
+        # handle case where key does not exist in LL
+        if target_ll.contains(key) is None:
+            return None
+
+        # handle case where key does exist within the LL
+        if target_ll.contains(key) is not None:
+            return target_ll.contains(key).value
 
     def put(self, key: str, value: object) -> None:
         """
-        Updates the key / value pair in the hash map. If a given key already exists
-        in the hash map, its associated value should be replaced with the new value.
-        If a given key is not in the hash map, a key / value pair should be added.
+        Updates the key / value pair in the HashMap
         """
-        pass
+        # pass key through hash function to find appropriate index in DA
+        hash_val = self.hash_function(key)
+        da_index = hash_val % self.capacity
+
+        # handle case where key already exists in LL
+        if self.buckets.get_at_index(da_index).contains(key) is not None:
+            self.buckets.get_at_index(da_index).remove(key)
+            self.buckets.get_at_index(da_index).insert(key, value)
+            return
+
+        # handle case where key does NOT exist in LL
+        if self.buckets.get_at_index(da_index).contains(key) is None:
+            self.buckets.get_at_index(da_index).insert(key, value)
+            self.size += 1
 
     def remove(self, key: str) -> None:
         """
-        Removes the given key and its associated value from the hash map
+        Removes the given key and its associated value from the HashMap
         """
-        pass
+        # pass key through hash function to find appropriate index in DA
+        hash_val = self.hash_function(key)
+        da_index = hash_val % self.capacity
+
+        # remove SLNode containing key if one exists
+        if self.buckets.get_at_index(da_index).remove(key):
+            # update HashMap.size if node removal was successful
+            self.size -= 1
 
     def contains_key(self, key: str) -> bool:
         """
-        Returns True if the given key is in the hash map, False otherwise
+        Returns True if the given key is in the HashMap, False otherwise
         """
-        return False
+        # pass key through hash function to find appropriate index in DA
+        hash_val = self.hash_function(key)
+        da_index = hash_val % self.capacity
+
+        # handle case where HashMap is empty
+        if self.buckets.length() == 0:
+            return False
+
+        # reference target LL with convenient variable
+        current_ll = self.buckets.get_at_index(da_index)
+
+        # handle case where key does not exists in target LL
+        if current_ll.contains(key) is None:
+            return False
+
+        # handle case where key exists in target LL
+        if current_ll.contains(key) is not None:
+            return True
 
     def empty_buckets(self) -> int:
         """
         Returns the number of empty buckets in the hash table
         """
-        return 0
+        # iterate through HashMap.buckets DA and count empty LinkedLists
+        count = 0
+        for index in range(self.buckets.length()):
+            if self.buckets.get_at_index(index).length() == 0:
+                count += 1
+        return count
 
     def table_load(self) -> float:
         """
-        Returns the current hash table load factor
+        Returns the current HashMap table load factor
         """
-        return 0.0
+        return float(self.size / self.capacity)
 
     def resize_table(self, new_capacity: int) -> None:
         """
-        Changes the capacity of the internal hash table. All existing key / value
-        pairs must remain in the new hash map and all hash links must be rehashed.
+        Changes the capacity of the internal HashMap table
         """
-        pass
+        # handle case where new_capacity < 1
+        if new_capacity < 1:
+            return
+
+        # construct new HashMap DA skeleton with new capacity
+        new_da = DynamicArray()
+        for _ in range(new_capacity):
+            new_da.append(LinkedList())
+
+        # iterate through original DA
+        for index in range(self.capacity):
+            # iterate through LL located at index
+            current_ll = self.buckets.get_at_index(index)
+            # re-hash keys and populate new_da with re-hashed nodes
+            for node in current_ll:
+                hash_val = self.hash_function(node.key)
+                new_da_index = hash_val % new_capacity
+                new_da.get_at_index(new_da_index).insert(node.key, node.value)
+
+        # replace HashMap.buckets w/ new_da and update HashMap.capacity
+        self.buckets = new_da
+        self.capacity = new_capacity
 
     def get_keys(self) -> DynamicArray:
         """
-        Returns a DynamicArray that contains all keys stored in your hash map
+        Returns a DynamicArray that contains all keys stored in my HashMap
         """
-        return DynamicArray()
+        # reference return DynamicArray with a convenient variable
+        return_da = DynamicArray()
+
+        # iterate through HashMap DA and LL while appending keys to return_da
+        for index in range(self.capacity):
+            current_ll = self.buckets.get_at_index(index)
+            for node in current_ll:
+                return_da.append(node.key)
+
+        return return_da
 
 
 # BASIC TESTING
